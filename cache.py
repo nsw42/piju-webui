50dbd4c4577ddf3f894ea08725af6735c8f0884e
Neil Walker <neil@wynded.co.uk>
2022-12-30 23:38:50 +0000

    Sort methods alphabetically

--- cache.py
+++ cache.py
@@ -25,18 +25,68 @@ class Cache:
         self.app = app
         self.flush()
 
-    def flush(self):
-        # The following instance variables are populated by ensure_genre_cache()
-        self.display_genres = None
-        self.genre_links = None
-        self.genre_names_from_links = {}
-        # The following instance variables are populated by ensure_genre_contents_cache(genre_name)
-        self.albums_in_genre = defaultdict(list)  # map from genre_name to list of Album
-        self.partial_cache = {}
-        # The following instance variables are populated by ensure_album_cache(album_id)
-        self.album_details = {}
-        # The following instance variables are populated by ensure_artist_cache(artist)
-        self.artist_details = {}
+    def add_album_from_json(self, album_json):
+        album_id = id_from_link(album_json['link'])
+        first_genre = album_json['genres'][0] if album_json['genres'] else None
+        genre_name = self.genre_names_from_links[first_genre] if first_genre else None
+        is_compilation = album_json['iscompilation']
+        artist = album_json['artist']
+        if not artist:
+            artist = 'Various Artists' if is_compilation else 'Unknown Artist'
+        anchor = artist[0].upper()
+        anchor = unidecode.unidecode(anchor)
+        if anchor not in string.ascii_uppercase:
+            anchor = 'num'
+        tracks = [Track(id=id_from_link(track_json['link']),
+                        title=track_json['title'],
+                        disknumber=track_json['disknumber'],
+                        tracknumber=track_json['tracknumber'])
+                  for track_json in album_json['tracks']]
+        tracks.sort(key=lambda t: (t.disknumber if (t.disknumber is not None) else 9999,
+                                   t.tracknumber if (t.tracknumber is not None) else 0))
+        album_details = Album(id=album_id,
+                              artist=artist,
+                              title=album_json['title'],
+                              year=album_json['releasedate'],
+                              artwork_link=album_json['artwork']['link'],
+                              is_compilation=is_compilation,
+                              genre_name=genre_name,
+                              numberdisks=album_json['numberdisks'],
+                              tracks=tracks,
+                              anchor=anchor)
+        self.album_details[album_id] = album_details
+        return album_details
+
+    def add_artist_from_json(self, artist_json):
+        # We'd normally only expect a single artist in the response
+        # but this works if there are multiple, which can happen if
+        # there are multiple capitalisations of an artist
+        albums = []
+        for artist_name, albums_json in artist_json.items():
+            for album_json in albums_json:
+                albums.append(self.add_album_from_json(album_json))
+        albums.sort(key=lambda album: album.year if album.year else 9999)
+        artist = Artist(artist_name, albums)
+        self.artist_details[artist_name.lower()] = artist
+
+    def ensure_album_cache(self, album_id) -> Optional[Album]:
+        self.ensure_genre_cache()  # Needed for the genre_name in add_album_from_json
+        if self.album_details.get(album_id) is None:
+            response = requests.get(f'{self.app.server}/albums/{album_id}?tracks=all')
+            if response.status_code != 200:
+                abort(500)  # TODO: Error handling
+            self.add_album_from_json(response.json())  # updates self.album_details[album_id]
+        return self.album_details[album_id]
+
+    def ensure_artist_cache(self, artist) -> Optional[Artist]:
+        self.ensure_genre_cache()  # Needed for the genre_name in add_album_from_json
+        artist_lookup = artist.lower()
+        if self.artist_details.get(artist_lookup) is None:
+            response = requests.get(f'{self.app.server}/artists/{artist}?tracks=all')
+            if response.status_code != 200:
+                abort(404)  # TODO: Error handling
+            self.add_artist_from_json(response.json())  # updates self.artist_details[artist.lower()]
+        return self.artist_details[artist_lookup]
 
     def ensure_genre_cache(self):
         self.app.logger.debug("ensure_genre_cache")
@@ -110,65 +160,15 @@ class Cache:
             self.albums_in_genre[genre_name] = albums
         return self.albums_in_genre[genre_name]
 
-    def ensure_album_cache(self, album_id) -> Optional[Album]:
-        self.ensure_genre_cache()  # Needed for the genre_name in add_album_from_json
-        if self.album_details.get(album_id) is None:
-            response = requests.get(f'{self.app.server}/albums/{album_id}?tracks=all')
-            if response.status_code != 200:
-                abort(500)  # TODO: Error handling
-            self.add_album_from_json(response.json())  # updates self.album_details[album_id]
-        return self.album_details[album_id]
-
-    def ensure_artist_cache(self, artist) -> Optional[Artist]:
-        self.ensure_genre_cache()  # Needed for the genre_name in add_album_from_json
-        artist_lookup = artist.lower()
-        if self.artist_details.get(artist_lookup) is None:
-            response = requests.get(f'{self.app.server}/artists/{artist}?tracks=all')
-            if response.status_code != 200:
-                abort(404)  # TODO: Error handling
-            self.add_artist_from_json(response.json())  # updates self.artist_details[artist.lower()]
-        return self.artist_details[artist_lookup]
-
-    def add_album_from_json(self, album_json):
-        album_id = id_from_link(album_json['link'])
-        first_genre = album_json['genres'][0] if album_json['genres'] else None
-        genre_name = self.genre_names_from_links[first_genre] if first_genre else None
-        is_compilation = album_json['iscompilation']
-        artist = album_json['artist']
-        if not artist:
-            artist = 'Various Artists' if is_compilation else 'Unknown Artist'
-        anchor = artist[0].upper()
-        anchor = unidecode.unidecode(anchor)
-        if anchor not in string.ascii_uppercase:
-            anchor = 'num'
-        tracks = [Track(id=id_from_link(track_json['link']),
-                        title=track_json['title'],
-                        disknumber=track_json['disknumber'],
-                        tracknumber=track_json['tracknumber'])
-                  for track_json in album_json['tracks']]
-        tracks.sort(key=lambda t: (t.disknumber if (t.disknumber is not None) else 9999,
-                                   t.tracknumber if (t.tracknumber is not None) else 0))
-        album_details = Album(id=album_id,
-                              artist=artist,
-                              title=album_json['title'],
-                              year=album_json['releasedate'],
-                              artwork_link=album_json['artwork']['link'],
-                              is_compilation=is_compilation,
-                              genre_name=genre_name,
-                              numberdisks=album_json['numberdisks'],
-                              tracks=tracks,
-                              anchor=anchor)
-        self.album_details[album_id] = album_details
-        return album_details
-
-    def add_artist_from_json(self, artist_json):
-        # We'd normally only expect a single artist in the response
-        # but this works if there are multiple, which can happen if
-        # there are multiple capitalisations of an artist
-        albums = []
-        for artist_name, albums_json in artist_json.items():
-            for album_json in albums_json:
-                albums.append(self.add_album_from_json(album_json))
-        albums.sort(key=lambda album: album.year if album.year else 9999)
-        artist = Artist(artist_name, albums)
-        self.artist_details[artist_name.lower()] = artist
+    def flush(self):
+        # The following instance variables are populated by ensure_genre_cache()
+        self.display_genres = None
+        self.genre_links = None
+        self.genre_names_from_links = {}
+        # The following instance variables are populated by ensure_genre_contents_cache(genre_name)
+        self.albums_in_genre = defaultdict(list)  # map from genre_name to list of Album
+        self.partial_cache = {}
+        # The following instance variables are populated by ensure_album_cache(album_id)
+        self.album_details = {}
+        # The following instance variables are populated by ensure_artist_cache(artist)
+        self.artist_details = {}
