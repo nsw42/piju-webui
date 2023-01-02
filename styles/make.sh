e85b31d5704c48eab4a59836c2309a9039f9840e
Neil Walker <neil@wynded.co.uk>
2023-01-02 11:28:41 +0000

    Implement (non-selectable) dark theme

--- styles/make.sh
+++ styles/make.sh
@@ -7,4 +7,4 @@ else
 fi
 
 scssdir=$(dirname $0)
-sass $watch $scssdir/pijuwebui.sass $scssdir/../static/pijuwebui.css
+sass $watch $scssdir/pijuwebui-light.sass:$scssdir/../static/pijuwebui-light.css   $scssdir/pijuwebui-dark.sass:$scssdir/../static/pijuwebui-dark.css
