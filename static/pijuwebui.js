12730d03a71413b6dea9676cd832aeded3c1aa61
Neil Walker <neil@wynded.co.uk>
2023-03-05 11:06:06 +0000

    More codeConvention renaming

--- static/pijuwebui.js
+++ static/pijuwebui.js
@@ -10,64 +10,68 @@ const STATE_PLAYING = 1;
 const STATE_PAUSED = 2;
 
 // Globals
-current_state = STATE_STOPPED;
-current_track_id = null;
-current_mode_remote_control_str = Cookies.get('mode');
-current_mode_remote_control = (current_mode_remote_control_str === undefined || current_mode_remote_control_str == 'remote');
+// Vars common to both local and remote mode
+currentTrackId = null;
+currentModeRemoteControlStr = Cookies.get('mode');
+currentModeRemoteControl = (currentModeRemoteControlStr === undefined || currentModeRemoteControlStr == 'remote');
+// Vars only relevant for remote control
+remoteCurrentState = STATE_STOPPED;
+// Vars only releveant for local playback
 localPlayers = null;
 localTrackIndex = null;
 fetching = false;
-// playlist_track_ids is also declared in script blocks in the html pages that support local playback
+// playlistTrackIds is also declared in script blocks in the html pages that support local playback
 
 setInterval(function() {
-    if (!current_mode_remote_control) {
+    if (!currentModeRemoteControl) {
         return;
     }
     // update nowplaying
     $.ajax({
         url: server,
         success: function(result) {
-            if (!current_mode_remote_control) {
+            if (!currentModeRemoteControl) {
                 // The mode may have changed while we were waiting for the response.
                 // Seems like an edge case, but I saw it during testing.
                 return;
             }
             result = JSON.parse(result);
-            current_track = result['CurrentTrack'];
-            if (current_track && Object.keys(current_track).length > 0) {
-                album_id = idFromLink(current_track['album']);
-                if (album_id != "") {
-                    album_link = "/albums/" + album_id;
-                    $("#now_playing_album_link").attr('href', album_link);
+            let currentTrack = result['CurrentTrack'];
+            let newState, newTrackId;
+            if (currentTrack && Object.keys(currentTrack).length > 0) {
+                let albumId = idFromLink(currentTrack['album']);
+                if (albumId != "") {
+                    let albumLink = "/albums/" + albumId;
+                    $("#now_playing_album_link").attr('href', albumLink);
                 }
-                artwork_link = current_track['artwork'];
-                if (artwork_link) {
+                let artworkLink = currentTrack['artwork'];
+                if (artworkLink) {
                     $("#now_playing_artwork_padding").addClass("d-none");
-                    $("#now_playing_artwork").removeClass("d-none").attr('src', server + artwork_link);
+                    $("#now_playing_artwork").removeClass("d-none").attr('src', server + artworkLink);
                 } else {
                     $("#now_playing_artwork_padding").removeClass("d-none");
                     $("#now_playing_artwork").addClass("d-none");
                 }
-                $("#now_playing_artist").text(current_track['artist']);
-                $("#now_playing_track").text(current_track['title']);
+                $("#now_playing_artist").text(currentTrack['artist']);
+                $("#now_playing_track").text(currentTrack['title']);
 
-                new_state = (result['PlayerStatus'] == "paused") ? STATE_PAUSED : STATE_PLAYING;
+                newState = (result['PlayerStatus'] == "paused") ? STATE_PAUSED : STATE_PLAYING;
 
-                new_track_id = idFromLink(current_track['link']);
+                newTrackId = idFromLink(currentTrack['link']);
             } else {
-                new_state = STATE_STOPPED;
-                new_track_id = null;
+                newState = STATE_STOPPED;
+                newTrackId = null;
             }
 
-            if (new_state != current_state) {
-                if (new_state == STATE_STOPPED) {
+            if (newState != remoteCurrentState) {
+                if (newState == STATE_STOPPED) {
                     $("#footer_nothing_playing").removeClass("d-none");
                     $("#footer_playing").addClass("d-none");
                 } else {
                     $("#footer_nothing_playing").addClass("d-none");
                     $("#footer_playing").removeClass("d-none");
 
-                    if (new_state == STATE_PAUSED) {
+                    if (newState == STATE_PAUSED) {
                         $("#now_playing_pause").addClass("d-none");
                         $("#now_playing_resume").removeClass("d-none");
                     } else {
@@ -76,13 +80,13 @@ setInterval(function() {
                     }
                 }
 
-                current_state = new_state;
+                remoteCurrentState = newState;
             }
 
-            if (new_track_id != current_track_id) {
-                $("#track_"+current_track_id).removeClass("active-track");
-                current_track_id = new_track_id;
-                $("#track_"+current_track_id).addClass("active-track");
+            if (newTrackId != currentTrackId) {
+                $("#track_"+currentTrackId).removeClass("active-track");
+                currentTrackId = newTrackId;
+                $("#track_"+currentTrackId).addClass("active-track");
             }
         }
     });
@@ -99,20 +103,20 @@ function idFromLink(link) {
     return tmp[tmp.length - 1];
 }
 
-function play_album(album_id, track_id, playlistIndex) {
-    if (current_mode_remote_control) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("POST", "/play_album/" + album_id + "/" + track_id, true);
+function playAlbum(albumId, trackId, playlistIndex) {
+    if (currentModeRemoteControl) {
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "/play_album/" + albumId + "/" + trackId, true);
         xhttp.send();
     } else {
         localPlay(playlistIndex);
     }
 }
 
-function play_playlist(playlist_id, track_id, playlistIndex) {
-    if (current_mode_remote_control) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("POST", "/play_playlist/" + playlist_id + "/" + track_id, true);
+function playPlaylist(playlistId, trackId, playlistIndex) {
+    if (currentModeRemoteControl) {
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "/play_playlist/" + playlistId + "/" + trackId, true);
         xhttp.send();
     } else {
         localPlay(playlistIndex);
@@ -120,8 +124,8 @@ function play_playlist(playlist_id, track_id, playlistIndex) {
 }
 
 function setupLocalPlayers() {
-    localPlayers = playlist_track_ids.map(track_id => new Howl({
-        src: [server + '/mp3/' + track_id],
+    localPlayers = playlistTrackIds.map(trackId => new Howl({
+        src: [server + '/mp3/' + trackId],
         preload: false,
         autoplay: false,
         format: ["mp3"],
@@ -130,12 +134,12 @@ function setupLocalPlayers() {
             console.log("howlerjs reported error code " + errorCode);
         },
         onend: function() {
-            $('#track_'+track_id).removeClass('active-track');
-            if (localTrackIndex + 1 < playlist_track_ids.length) {
+            $('#track_'+trackId).removeClass('active-track');
+            if (localTrackIndex + 1 < playlistTrackIds.length) {
                 localPlay(localTrackIndex + 1);
             } else {
                 hideButtons(['#local-previous', '#local-pause', '#local-fetching', '#local-resume', '#local-next']);
-                current_track_id = localTrackIndex = null;
+                currentTrackId = localTrackIndex = null;
             }
         },
         onpause: function() {
@@ -156,14 +160,14 @@ function localPlay(playlistIndex) {
     }
     let setMediaHandlers = true;
     if (localTrackIndex != null) {
-        $("#track_"+current_track_id).removeClass('active-track');
+        $("#track_"+currentTrackId).removeClass('active-track');
         localPlayers[localTrackIndex].stop();
         setMediaHandlers = false;  // Setting the same handlers a second time bizarrely seems to stop the handlers being called
     }
     navigator.mediaSession.setPositionState({duration: 1000000, position: 0}); // Simulate an incredibly long track so 'fast forward' will always be invoked, and we can map it to 'next track'
     localTrackIndex = playlistIndex;
-    current_track_id = playlist_track_ids[playlistIndex];
-    $("#track_"+current_track_id).addClass('active-track');
+    currentTrackId = playlistTrackIds[playlistIndex];
+    $("#track_"+currentTrackId).addClass('active-track');
     localPlayers[playlistIndex].play();
     showPlaybackActive();
     if (setMediaHandlers) {
@@ -177,7 +181,7 @@ function localPlay(playlistIndex) {
         }
     }
     $('#local-previous').prop('disabled', (localTrackIndex == 0));
-    $('#local-next').prop('disabled', (localTrackIndex + 1 >= playlist_track_ids.length));
+    $('#local-next').prop('disabled', (localTrackIndex + 1 >= playlistTrackIds.length));
     showPlaybackFetching();
     fetching = true;
 }
@@ -198,7 +202,7 @@ function localPrevious() {
 }
 
 function localNext() {
-    if (localTrackIndex + 1 < playlist_track_ids.length) {
+    if (localTrackIndex + 1 < playlistTrackIds.length) {
         localPause();  // Avoids synchronisation problems on multiple skips
         localPlay(localTrackIndex + 1);
     }
@@ -230,14 +234,14 @@ function showButtons(allToShow) {
 }
 
 function showPlaybackFetching() {
-    $('#local-fetching').removeClass('d-none');
+    showButtons(['#local-fetching']);
     hideButtons(['#local-previous', '#local-pause', '#local-resume', '#local-next']);
 }
 
 function showPlaybackPaused(resumable) {
     hideButtons(['#local-fetching', '#local-pause']);
     if (resumable) {
-        $('#local-resume').removeClass('d-none');
+        showButtons(['#local-resume']);
         navigator.mediaSession.playbackState = "paused";
     } else {
         navigator.mediaSession.playbackState = "none";
@@ -250,41 +254,41 @@ function showPlaybackActive() {
     showButtons(['#local-previous', '#local-pause', '#local-next']);
 }
 
-function send_pause() {
+function sendPause() {
     $.ajax({
         url: server + "/player/pause",
         method: "POST"
     });
 }
 
-function send_resume() {
+function sendResume() {
     $.ajax({
         url: server + "/player/resume",
         method: "POST"
     });
 }
 
-function togglemode() {
-    current_mode_remote_control = !current_mode_remote_control;
-    Cookies.set('mode', current_mode_remote_control ? 'remote' : 'local');
+function toggleMode() {
+    currentModeRemoteControl = !currentModeRemoteControl;
+    Cookies.set('mode', currentModeRemoteControl ? 'remote' : 'local');
     $('#mode-indicator-remote').toggleClass('d-none');
     $('#mode-indicator-local').toggleClass('d-none');
 
-    if (current_track_id != null) {
-        $("#track_"+current_track_id).removeClass('active-track');
+    if (currentTrackId != null) {
+        $("#track_"+currentTrackId).removeClass('active-track');
     }
 
-    if (current_mode_remote_control) {
+    if (currentModeRemoteControl) {
         localPause(false);
-        current_state = STATE_UNKNOWN;  // Ensure display updates when the timer next fires
-        current_track_id = null;  // ditto
+        remoteCurrentState = STATE_UNKNOWN;  // Ensure display updates when the timer next fires
+        currentTrackId = null;  // ditto
         $('#footer_nothing_playing').removeClass('d-none');
         $('#footer_playing').removeClass('d-none');
         $('#footer-local-playback').addClass('d-none');
     } else {
         if (localTrackIndex != null) {
-            current_track_id = playlist_track_ids[localTrackIndex];
-            $("#track_"+current_track_id).addClass('active-track');
+            currentTrackId = playlistTrackIds[localTrackIndex];
+            $("#track_"+currentTrackId).addClass('active-track');
             localResume();
         }
         $('#footer_nothing_playing').addClass('d-none');
