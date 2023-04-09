d653000e66643435303dae3bf0c12a867de00b9f
Neil Walker <neil@wynded.co.uk>
2023-04-09 15:24:29 +0100

    Bump required API version to 4.1

--- main.py
+++ main.py
@@ -283,7 +283,7 @@ def main():
     app.dev_reload = args.dev_reload
     app.server = args.server
     app.cache = Cache(app)
-    connection_test(app.server, required_api_version='4.0')
+    connection_test(app.server, required_api_version='4.1')
     host, port = '0.0.0.0', 80
     if args.dev_reload:
         app.run(host=host, port=port, debug=True)
