81c24efdafb650d39908045b0c1e586ca6264fef
Neil Walker <neil@wynded.co.uk>
2023-04-10 18:15:32 +0100

    Further improvements to the marquee

--- styles/make_marquee.py
+++ styles/make_marquee.py
@@ -1,12 +1,28 @@
-INDENT='    '
+from collections import namedtuple
+
+INDENT = 4 * ' '
 SLIDE_DURATION = 2
 SHOW = 4
 
+Keyframe = namedtuple('Keyframe', 'time, xpos, opacity')
 
 # based on https://www.hongkiat.com/blog/css3-animation-advanced-marquee/
 
+
+def write_keyframes(n, i, total_s, keyframes):
+    print(f'@keyframes marquee-{n}elt-{i}')
+    for (s, xpos, opacity) in keyframes:
+        pct = int(round(100 * s / total_s))
+        print(f'{INDENT}/* {s} */')
+        print(f'{INDENT}{pct}%')
+        print(f'{INDENT}{INDENT}transform: translateX({xpos}%)')
+        if opacity is not None:
+            print(f'{INDENT}{INDENT}opacity: {opacity}')
+    print()
+
+
 def make_marquee(n):
-    total_s = n * SHOW + (n + 1) * SLIDE_DURATION
+    total_s = n * (SHOW + SLIDE_DURATION)
     print(f'.marquee-{n}elt')
     print(f'{INDENT}@extend %marquee')
     print()
@@ -15,30 +31,35 @@ def make_marquee(n):
         print(f'{INDENT}{INDENT}animation: marquee-{n}elt-{i} {total_s}s ease infinite')
         print()
 
+    # Example for two lines of text, showing the special requirements for the first line of text
+    #           0s          4s          6s         10s        12s
+    # text1:    ||  show    |  slide out |         | slide in  ||  show
+    # text2:    ||          |  slide in  | show    | slide out ||
+
     for i in range(1, n + 1):
         keyframes = []
-        slide_in_start_at_s = (i-1) * (SLIDE_DURATION + SHOW)
-        if slide_in_start_at_s > 0:
-            keyframes.append((0, 100))
-        keyframes.append((slide_in_start_at_s, 100))
+        if i == 1:
+            slide_in_start_at_s = total_s - SLIDE_DURATION
+            keyframes.append(Keyframe(time=0, xpos=0, opacity=1))
+        else:
+            slide_in_start_at_s = (i - 2) * SLIDE_DURATION + (i - 1) * SHOW
+            keyframes.append(Keyframe(time=0, xpos=100, opacity=0))
+        keyframes.append(Keyframe(time=slide_in_start_at_s - 1, xpos=100, opacity=0))
+        keyframes.append(Keyframe(time=slide_in_start_at_s, xpos=100, opacity=1))
         slide_in_end_at_s = slide_in_start_at_s + SLIDE_DURATION
-        keyframes.append((slide_in_end_at_s, 0))
-        show_end_at_s = slide_in_end_at_s + SHOW
-        keyframes.append((show_end_at_s, 0))
+        keyframes.append(Keyframe(time=slide_in_end_at_s, xpos=0, opacity=1))
+        show_end_at_s = SHOW if (i == 1) else (slide_in_end_at_s + SHOW)
+        keyframes.append(Keyframe(time=show_end_at_s, xpos=0, opacity=None))
         slide_out_end_at_s = show_end_at_s + SLIDE_DURATION
-        keyframes.append((slide_out_end_at_s, -100))
+        keyframes.append(Keyframe(time=slide_out_end_at_s, xpos=-100, opacity=1))
         if slide_out_end_at_s < total_s:
-            keyframes.append((total_s, -100))
+            keyframes.append(Keyframe(time=slide_out_end_at_s + 1, xpos=-100, opacity=0))
+
+            if i > 1:
+                keyframes.append(Keyframe(time=total_s, xpos=-100, opacity=0))
 
         keyframes.sort()
-
-        print(f'@keyframes marquee-{n}elt-{i}')
-        for (s, xpos) in keyframes:
-            pct = int(round(100 * s / total_s))
-            print(f'{INDENT}/* {s} */')
-            print(f'{INDENT}{pct}%')
-            print(f'{INDENT}{INDENT}transform: translateX({xpos}%)')
-        print()
+        write_keyframes(n, i, total_s, keyframes)
 
 
 if __name__ == '__main__':
