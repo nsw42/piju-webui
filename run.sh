9dfdfb8342e202139c2d970bfe253f8a4655c7e4
Neil Walker <neil@wynded.co.uk>
2023-10-02 19:38:04 +0100

    Ensure external dependencies are fetched at startup

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 
+WEBUI_DIR="$(dirname $0)"
+${WEBUI_DIR}/fetch_external.sh
+
 while true; do
-  /usr/bin/python3 $(dirname $0)/main.py "$@"
+  /usr/bin/python3 ${WEBUI_DIR}/main.py "$@"
   if [ $? -ne 0 ] ; then break; fi
 done
