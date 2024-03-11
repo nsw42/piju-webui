778d17bafa017bef6ff95e726fdb3c67f1d2f6ee
Neil Walker <neil@wynded.co.uk>
2024-03-11 18:04:41 +0000

    Refactoring/code tidying

--- static/pijuwebui.js
+++ static/pijuwebui.js
@@ -8,18 +8,18 @@ const STATE_UNKNOWN = -1;
 const STATE_STOPPED = 0;
 const STATE_PLAYING = 1;
 const STATE_PAUSED = 2;
+const currentModeRemoteControlAtStart = Cookies.get('mode');
 
 // Globals
 // Vars common to both local and remote mode
-currentTrackId = null;
-currentModeRemoteControlStr = Cookies.get('mode');
-currentModeRemoteControl = (currentModeRemoteControlStr === undefined || currentModeRemoteControlStr == 'remote');
+let currentTrackId = null;
+let currentModeRemoteControl = (currentModeRemoteControlAtStart === undefined || currentModeRemoteControlAtStart == 'remote');
 // Vars only relevant for remote control
-remoteCurrentState = STATE_STOPPED;
+let remoteCurrentState = STATE_STOPPED;
 // Vars only releveant for local playback
-localPlayers = null;
-localTrackIndex = null;
-fetching = false;
+let localPlayers = null;
+let localTrackIndex = null;
+let fetching = false;
 // playlistTrackIds is also declared in script blocks in the html pages that support local playback
 
 // An ontouchstart event listener is needed to allow visual feedback
@@ -94,93 +94,97 @@ setInterval(function() {
     $.ajax({
         url: server,
         success: function(result) {
-            if (!currentModeRemoteControl) {
-                // The mode may have changed while we were waiting for the response.
-                // Seems like an edge case, but I saw it during testing.
-                return;
+            const json = JSON.parse(result)
+            showNowPlaying(json)
+        },
+    })
+}, 1000)
+
+function showNowPlaying(nowPlaying) {
+    if (!currentModeRemoteControl) {
+        // The mode may have changed while we were waiting for the response.
+        // Seems like an edge case, but I saw it during testing.
+        return;
+    }
+    let newState, newTrackId;
+    if (nowPlaying['PlayerStatus'] == 'stopped') {
+        newState = STATE_STOPPED;
+        newTrackId = null;
+    } else {
+        const currentTrack = nowPlaying['CurrentTrack'];
+        const tracklistSource = nowPlaying['CurrentTracklistUri'];
+        if (tracklistSource === undefined) {
+            // If we're streaming, disable the apparent clickability of the 'now playing' album link
+            $('#now_playing_album_link').removeAttr('href');
+        } else {
+            $("#now_playing_album_link").attr('href', tracklistSource);
+        }
+
+        // Handle artwork
+        let artworkLink = nowPlaying['CurrentArtwork'];
+        if (artworkLink) {
+            if (!artworkLink.startsWith('http')) {
+                artworkLink = server + artworkLink;
             }
-            result = JSON.parse(result);
-            let newState, newTrackId;
-            if (result['PlayerStatus'] == 'stopped') {
-                newState = STATE_STOPPED;
-                newTrackId = null;
+            $("#now_playing_artwork_padding").addClass("d-none");
+            $("#now_playing_artwork").removeClass("d-none").attr('src', artworkLink);
+        } else {
+            $("#now_playing_artwork_padding").removeClass("d-none");
+            $("#now_playing_artwork").addClass("d-none");
+        }
+
+        // Information about what is playing
+        if (currentTrack === undefined) {
+            $("#now_playing_artist").text(nowPlaying['CurrentStream']);
+            $("#now_playing_track").text("");
+            newTrackId = undefined;
+        } else {
+            $("#now_playing_artist").text(currentTrack['artist']);
+            $("#now_playing_track").text(currentTrack['title']);
+            newTrackId = idFromLink(currentTrack['link']);
+        }
+
+        newState = (nowPlaying['PlayerStatus'] == "paused") ? STATE_PAUSED : STATE_PLAYING;
+    }
+
+    if (newState != remoteCurrentState) {
+        if (newState == STATE_STOPPED) {
+            $("#footer_nothing_playing").removeClass("d-none");
+            $("#footer_playing").addClass("d-none");
+        } else {
+            $("#footer_nothing_playing").addClass("d-none");
+            $("#footer_playing").removeClass("d-none");
+
+            if (newState == STATE_PAUSED) {
+                $("#now_playing_pause").addClass("d-none");
+                $("#now_playing_resume").removeClass("d-none");
             } else {
-                let currentTrack = result['CurrentTrack'];
-                let tracklistSource = result['CurrentTracklistUri'];
-                if (tracklistSource === undefined) {
-                    // If we're streaming, disable the apparent clickability of the 'now playing' album link
-                    $('#now_playing_album_link').removeAttr('href');
-                } else {
-                    $("#now_playing_album_link").attr('href', tracklistSource);
-                }
-
-                // Handle artwork
-                let artworkLink = result['CurrentArtwork'];
-                if (artworkLink) {
-                    if (!artworkLink.startsWith('http')) {
-                        artworkLink = server + artworkLink;
-                    }
-                    $("#now_playing_artwork_padding").addClass("d-none");
-                    $("#now_playing_artwork").removeClass("d-none").attr('src', artworkLink);
-                } else {
-                    $("#now_playing_artwork_padding").removeClass("d-none");
-                    $("#now_playing_artwork").addClass("d-none");
-                }
-
-                // Information about what is playing
-                if (currentTrack === undefined) {
-                    $("#now_playing_artist").text(result['CurrentStream']);
-                    $("#now_playing_track").text("");
-                    newTrackId = undefined;
-                } else {
-                    $("#now_playing_artist").text(currentTrack['artist']);
-                    $("#now_playing_track").text(currentTrack['title']);
-                    newTrackId = idFromLink(currentTrack['link']);
-                }
-
-                newState = (result['PlayerStatus'] == "paused") ? STATE_PAUSED : STATE_PLAYING;
-            }
-
-            if (newState != remoteCurrentState) {
-                if (newState == STATE_STOPPED) {
-                    $("#footer_nothing_playing").removeClass("d-none");
-                    $("#footer_playing").addClass("d-none");
-                } else {
-                    $("#footer_nothing_playing").addClass("d-none");
-                    $("#footer_playing").removeClass("d-none");
-
-                    if (newState == STATE_PAUSED) {
-                        $("#now_playing_pause").addClass("d-none");
-                        $("#now_playing_resume").removeClass("d-none");
-                    } else {
-                        $("#now_playing_pause").removeClass("d-none");
-                        $("#now_playing_resume").addClass("d-none");
-                    }
-                }
-
-                remoteCurrentState = newState;
-            }
-
-            if (newTrackId != currentTrackId) {
-                $("#track_"+currentTrackId).removeClass("active-track");
-                let currentTrackNode = $("#track_"+newTrackId);
-                if (currentTrackNode.length == 0) {
-                    currentTrackId = null;  // queue page may be mid-refresh
-                } else {
-                    currentTrackId = newTrackId;
-                    currentTrackNode.addClass("active-track");
-                }
-            }
-
-            let downloadingState = result['WorkerStatus'];
-            if (downloadingState.startsWith("Fetching")) {
-                $("#downloading-indicator-parent").removeClass('d-none');
-            } else {
-                $("#downloading-indicator-parent").addClass('d-none');
+                $("#now_playing_pause").removeClass("d-none");
+                $("#now_playing_resume").addClass("d-none");
             }
         }
-    });
-}, 1000);
+
+        remoteCurrentState = newState;
+    }
+
+    if (newTrackId != currentTrackId) {
+        $("#track_"+currentTrackId).removeClass("active-track");
+        let currentTrackNode = $("#track_"+newTrackId);
+        if (currentTrackNode.length == 0) {
+            currentTrackId = null;  // queue page may be mid-refresh
+        } else {
+            currentTrackId = newTrackId;
+            currentTrackNode.addClass("active-track");
+        }
+    }
+
+    const downloadingState = nowPlaying['WorkerStatus'];
+    if (downloadingState.startsWith("Fetching")) {
+        $("#downloading-indicator-parent").removeClass('d-none');
+    } else {
+        $("#downloading-indicator-parent").addClass('d-none');
+    }
+}
 
 
 function sendPause() {
