f2f2ea6be72ac8d918686652055dddce0db31b66
Neil Walker <neil@wynded.co.uk>
2024-04-07 15:29:05 +0100

    JS tidying

--- static/genre.js
+++ static/genre.js
@@ -1,14 +1,14 @@
 $(function() {
-    var url = window.location.href;
-    var slash = url.lastIndexOf('/');
-    var leaf = url.substring(slash + 1);
+    let url = window.location.href;
+    let slash = url.lastIndexOf('/');
+    let leaf = url.substring(slash + 1);
     startAjaxRequest(leaf, 30000);
 });
 
-var genre_fetch_results = null;
+let genre_fetch_results = null;
 
 function startAjaxRequest(leaf, timeout) {
-    var now = new Date();
+    const now = new Date();
     console.log(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} starting ajax request (timeout=${timeout})`);
     $.ajax({
         url: "/genre_contents/" + leaf + '?timeout=' + timeout,
@@ -51,21 +51,21 @@ $('#random-toggle').change(function() {
 
 
 function show_albums(albums) {
-    var genre_content_node = document.getElementById("genre-content");
+    let genre_content_node = document.getElementById("genre-content");
     while (genre_content_node.hasChildNodes()) {
         genre_content_node.removeChild(genre_content_node.firstChild);
     }
-    var selected_anchors = {};
-    for (var album of albums) {
-        var album_anchor = album['anchor'];
+    let selected_anchors = {};
+    for (let album of albums) {
+        let album_anchor = album['anchor'];
         if (selected_anchors[album_anchor] == null) {
-            anchor = document.getElementById("anchor-template").content.cloneNode(true);
+            let anchor = document.getElementById("anchor-template").content.cloneNode(true);
             anchor.querySelector("a").setAttribute("name", album_anchor);
             genre_content_node.appendChild(anchor);
             selected_anchors[album_anchor] = true;
         }
 
-        var album_row = document.getElementById("album-template").content.cloneNode(true);
+        let album_row = document.getElementById("album-template").content.cloneNode(true);
         if (album['artwork_link'] != null) {
             // We have artwork: remove the spacer
             album_row.querySelector("#artwork-spacer").remove();
@@ -74,22 +74,21 @@ function show_albums(albums) {
             // No artwork: remove the img node
             album_row.querySelector("#album-artwork").remove();
         }
-        var album_href = album_row.querySelector("#album-href");
+        let album_href = album_row.querySelector("#album-href");
         album_href.setAttribute("href", `/albums/${album['id']}`);
         album_href.innerText = (album['artist'] != null ? album['artist'] : "Unknown Artist") + ": " + (album['title'] != null ? album['title'] : "Unknown Album");
         if (album['year'] != null) {
-            var album_year_node = album_row.querySelector("#album-year");
+            let album_year_node = album_row.querySelector("#album-year");
             album_year_node.innerText = " (" + album['year'] + ")";
         }
         genre_content_node.appendChild(album_row);
     }
     genre_content_node.classList.remove('d-none');
 
-    var letters = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var anchor_content_node = document.getElementById("anchor-content");
-    var anchors_contents ='';
-    for (var letter of letters) {
-        var anchor = (letter == '#' ? 'num' : letter);
+    const letters = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let anchors_contents ='';
+    for (let letter of letters) {
+        const anchor = (letter == '#' ? 'num' : letter);
         anchors_contents = anchors_contents.concat(
             '<div class="text-center">',
             selected_anchors[anchor] == null ? letter : `<a href="#${anchor}">${letter}</a>`,
@@ -117,7 +116,7 @@ function show_genre_subset() {
     if (all_albums.length <= nr_albums_to_show) {
         show_albums(all_albums);
     } else {
-        var indexes = [];
+        let indexes = [];
         while (indexes.length < nr_albums_to_show) {
             const random = Math.floor(Math.random() * all_albums.length);
             if (indexes.indexOf(random) !== -1) {
@@ -126,8 +125,8 @@ function show_genre_subset() {
             indexes.push(random);
         }
         indexes.sort(function(a, b){return a - b});
-        var to_show = [];
-        for (var index of indexes) {
+        let to_show = [];
+        for (let index of indexes) {
             to_show.push(all_albums[index]);
         }
         show_albums(to_show);
