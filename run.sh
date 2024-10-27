dfd5c211cbc8bd6aefe0f9841f6d72fa3fb5e40b
Neil Walker <neil@wynded.co.uk>
2024-10-27 11:35:44 +0000

    Support running in a venv

--- run.sh
+++ run.sh
@@ -2,8 +2,15 @@
 
 WEBUI_DIR="$(dirname $0)"
 
+if [ -x $WEBUI_DIR/bin/python3 ]; then
+  PYTHON=$WEBUI_DIR/bin/python3
+else
+  # rely on path
+  PYTHON=python3
+fi
+
 while true; do
   ${WEBUI_DIR}/fetch_external.sh
-  python3 ${WEBUI_DIR}/main.py "$@"
+  $PYTHON ${WEBUI_DIR}/main.py "$@"
   if [ $? -eq 0 ] ; then break; fi
 done
