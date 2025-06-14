e72ac6c1e4207104356c8108a795b90cfeb69bc0
nsw42 <neil@wynded.co.uk>
2025-06-14 19:45:45 +0100

    Use a slower marquee animation later in the day

--- styles/make_marquee.py
+++ styles/make_marquee.py
@@ -1,34 +1,38 @@
-from collections import namedtuple
+from dataclasses import dataclass
 
 INDENT = 4 * ' '
-SLIDE_DURATION = 2
-SHOW = 4
 
-Keyframe = namedtuple('Keyframe', 'time, xpos, opacity')
+
+@dataclass
+class Keyframe:
+    time: int
+    xpos: int
+    opacity: int
 
 # based on https://www.hongkiat.com/blog/css3-animation-advanced-marquee/
 
 
-def write_keyframes(n, i, total_s, keyframes):
-    print(f'@keyframes marquee-{n}elt-{i}')
-    for (s, xpos, opacity) in keyframes:
-        pct = int(round(100 * s / total_s))
-        print(f'{INDENT}/* {s} */')
+def write_keyframes(speed_name, n, i, total_s, keyframes):
+    print(f'@keyframes marquee-{speed_name}-{n}elt-{i}')
+    for kf in keyframes:
+        pct = int(round(100 * kf.time / total_s))
+        print(f'{INDENT}/* {kf.time} */')
         print(f'{INDENT}{pct}%')
-        print(f'{INDENT}{INDENT}transform: translateX({xpos}%)')
-        if opacity is not None:
-            print(f'{INDENT}{INDENT}opacity: {opacity}')
+        print(f'{INDENT}{INDENT}transform: translateX({kf.xpos}%)')
+        if kf.opacity is not None:
+            print(f'{INDENT}{INDENT}opacity: {kf.opacity}')
+        print(f'{INDENT}{INDENT}background-color: $headerfooter-background')
     print()
 
 
-def make_marquee(n):
-    total_s = n * (SHOW + SLIDE_DURATION)
-    print(f'.marquee-{n}elt')
+def make_marquee(speed_name, n, show_duration, slide_duration):
+    total_s = n * (show_duration + slide_duration)
+    print(f'.marquee-{speed_name} .marquee-{n}elt')
     print(f'{INDENT}@extend %marquee')
     print()
     for i in range(1, n + 1):
         print(f'{INDENT}div:nth-child({i})')
-        print(f'{INDENT}{INDENT}animation: marquee-{n}elt-{i} {total_s}s ease infinite')
+        print(f'{INDENT}{INDENT}animation: marquee-{speed_name}-{n}elt-{i} {total_s}s ease infinite')
         print()
 
     # Example for two lines of text, showing the special requirements for the first line of text
@@ -39,18 +43,18 @@ def make_marquee(n):
     for i in range(1, n + 1):
         keyframes = []
         if i == 1:
-            slide_in_start_at_s = total_s - SLIDE_DURATION
+            slide_in_start_at_s = total_s - slide_duration
             keyframes.append(Keyframe(time=0, xpos=0, opacity=1))
         else:
-            slide_in_start_at_s = (i - 2) * SLIDE_DURATION + (i - 1) * SHOW
+            slide_in_start_at_s = (i - 2) * slide_duration + (i - 1) * show_duration
             keyframes.append(Keyframe(time=0, xpos=100, opacity=0))
         keyframes.append(Keyframe(time=slide_in_start_at_s - 1, xpos=100, opacity=0))
         keyframes.append(Keyframe(time=slide_in_start_at_s, xpos=100, opacity=1))
-        slide_in_end_at_s = slide_in_start_at_s + SLIDE_DURATION
+        slide_in_end_at_s = slide_in_start_at_s + slide_duration
         keyframes.append(Keyframe(time=slide_in_end_at_s, xpos=0, opacity=1))
-        show_end_at_s = SHOW if (i == 1) else (slide_in_end_at_s + SHOW)
-        keyframes.append(Keyframe(time=show_end_at_s, xpos=0, opacity=None))
-        slide_out_end_at_s = show_end_at_s + SLIDE_DURATION
+        show_end_at_s = show_duration if (i == 1) else (slide_in_end_at_s + show_duration)
+        keyframes.append(Keyframe(time=show_end_at_s, xpos=0, opacity=1))
+        slide_out_end_at_s = show_end_at_s + slide_duration
         keyframes.append(Keyframe(time=slide_out_end_at_s, xpos=-100, opacity=1))
         if slide_out_end_at_s < total_s:
             keyframes.append(Keyframe(time=slide_out_end_at_s + 1, xpos=-100, opacity=0))
@@ -58,10 +62,11 @@ def make_marquee(n):
             if i > 1:
                 keyframes.append(Keyframe(time=total_s, xpos=-100, opacity=0))
 
-        keyframes.sort()
-        write_keyframes(n, i, total_s, keyframes)
+        keyframes.sort(key=lambda kf: kf.time)
+        write_keyframes(speed_name, n, i, total_s, keyframes)
 
 
 if __name__ == '__main__':
-    make_marquee(2)
-    make_marquee(3)
+    for n_elts in (2, 3):
+        make_marquee('fast', n_elts, show_duration=4, slide_duration=2)
+        make_marquee('slow', n_elts, show_duration=4, slide_duration=4)
