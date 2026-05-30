bd650f759d726714acedc334f7d602770c86a792
nsw42 <neil@wynded.co.uk>
2026-05-30 14:37:00 +0100

    Build bootstrap/bootswatch into the piju CSS file
    This isn't setting some of the Bootstrap overrides correctly - volume
    slider size, text in the All/Random toggle

--- styles/make.sh
+++ styles/make.sh
@@ -7,6 +7,15 @@ else
 fi
 
 scssdir=$(dirname $0)
+
+# ensure we have bootstrap source available
+
+if [ ! -d "$scssdir/../node_modules/bootstrap" -o ! -d "$scssdir/../node_modules/bootswatch" ]; then
+  echo "bootstrap or bootswatch missing - installing"
+  pnpm install
+fi
+
 # TODO: Honour --watch for the python, too
 python $scssdir/make_marquee.py > $scssdir/marquee.sass
-sass $watch $scssdir/pijuwebui-light.sass:$scssdir/../static/pijuwebui-light.css   $scssdir/pijuwebui-dark.sass:$scssdir/../static/pijuwebui-dark.css
+
+sass $watch -I $scssdir/../node_modules --silence-deprecation import $scssdir/pijuwebui-light.sass:$scssdir/../static/pijuwebui-light.css   $scssdir/pijuwebui-dark.sass:$scssdir/../static/pijuwebui-dark.css
